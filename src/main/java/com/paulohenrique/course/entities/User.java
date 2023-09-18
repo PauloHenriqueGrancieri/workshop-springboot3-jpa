2b7f9cf0f7a9ffd02f7328bab56728b76dec9929
Paulo Henrique Grancieri <paulohenriquegrancieri@gmail.com>
2023-09-18 15:53:51 -0300

    H2 database, test profile, jpa

--- src/main/java/com/paulohenrique/course/entities/User.java
+++ src/main/java/com/paulohenrique/course/entities/User.java
@@ -1,5 +1,6 @@
 package com.paulohenrique.course.entities;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
@@ -7,11 +8,24 @@ import java.io.Serializable;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "phone")
     private String phone;
+
+    @Column(name = "password")
     private String password;
 }
