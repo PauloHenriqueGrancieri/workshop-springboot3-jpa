07aa18024bd217a0dcad21a2b0f03b6285ffeb34
Paulo Henrique Grancieri <paulohenriquegrancieri@gmail.com>
2023-09-20 18:40:12 -0300

    Create Order

--- src/main/java/com/paulohenrique/course/config/TestConfig.java
+++ src/main/java/com/paulohenrique/course/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.paulohenrique.course.config;
 
+import com.paulohenrique.course.entities.Order;
 import com.paulohenrique.course.entities.User;
 import com.paulohenrique.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @Profile("test")
@@ -18,16 +21,20 @@ public class TestConfig implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        List<Order> orders = new ArrayList<>();
+
         User u1 = new User(null,
                 "Maria Brown",
                 "maria@gmail.com",
                 "988888888",
-                "123456");
+                "123456",
+                orders);
         User u2 = new User(null,
                 "Alex Green",
                 "alex@gmail.com",
                 "977777777",
-                "123456");
+                "123456",
+                orders);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
     }
--- src/main/java/com/paulohenrique/course/entities/Order.java
+++ src/main/java/com/paulohenrique/course/entities/Order.java
@@ -4,44 +4,34 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Instant;
 import java.util.Objects;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "tb_user")
-public class User implements Serializable {
+@Table(name = "tb_order")
+public class Order implements Serializable {
 
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name")
-    private String name;
+    @Column(name = "moment")
+    private Instant moment;
 
-    @Column(name = "email")
-    private String email;
-
-    @Column(name = "phone")
-    private String phone;
-
-    @Column(name = "password")
-    private String password;
-
-    @OneToMany(mappedBy = "client")
-    @Setter(AccessLevel.PRIVATE)
-    private List<Order> orders = new ArrayList<>();
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id);
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
     }
 
     @Override
