4e69f3a65ee615869f1a110153c82567fe30b737
Paulo Henrique Grancieri <paulohenriquegrancieri@gmail.com>
2023-09-22 15:40:27 -0300

    OrderItem

---
+++ src/main/java/com/paulohenrique/course/entities/pk/OrderItemPK.java
@@ -0,0 +1,38 @@
+package com.paulohenrique.course.entities.pk;
+
+import com.paulohenrique.course.entities.Order;
+import com.paulohenrique.course.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Data
+@Embeddable
+public class OrderItemPK implements Serializable {
+    private static final Long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+}
