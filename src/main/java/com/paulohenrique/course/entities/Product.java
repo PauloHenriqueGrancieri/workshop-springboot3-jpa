d8bf06ef9ef7648af0432dbbed815eff80f068b7
Paulo Henrique Grancieri <paulohenriquegrancieri@gmail.com>
2023-09-24 21:32:16 -0300

    Product-OrderItem

--- src/main/java/com/paulohenrique/course/entities/Product.java
+++ src/main/java/com/paulohenrique/course/entities/Product.java
@@ -1,5 +1,7 @@
 package com.paulohenrique.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -40,11 +42,24 @@ public class Product implements Serializable {
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    @OneToMany(mappedBy = "id.product")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Set<OrderItem> items = new HashSet<>();
+
     public Product(String name, String description, Double price, String imgUrl) {
         this.name = name;
         this.description = description;
         this.price = price;
         this.imgUrl = imgUrl;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        for(OrderItem x : items){
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
